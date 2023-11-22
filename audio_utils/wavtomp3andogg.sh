bbd02ef10f07d756a080369269f50fe9b8585c2b
JR <jadedResearcher@gmail.com>
2023-11-21 20:15:06 -0500

    initial commit

---
+++ audio_utils/wavtomp3andogg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for f in *.wav
+do
+  tempfile="${f##*/}"
+
+  ## display filename
+  fileName="${f%.*}"
+  echo "Processing $f file...I think it should be ${fileName}.ogg and mp3"
+
+  # take action on each file. $f store current file name
+  ffmpeg -i $f -acodec libvorbis ${fileName}.ogg
+  ffmpeg -i $f  ${fileName}.mp3
+
+
+done
